0e1a5e61580bbb0efb767d8889a804539ad913b5
prashastinanda <139529682+prashastinanda@users.noreply.github.com>
2025-06-08 13:35:05 +0530

    Delete src directory

--- src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-    return (
-      <div className="text-center mt-20">
-        <h1 className="text-4xl font-bold">404</h1>
-        <p className="text-lg text-gray-500">Page not found.</p>
-      </div>
-    );
-  }
-  
